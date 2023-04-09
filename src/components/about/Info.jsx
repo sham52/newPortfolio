50058c8e0bc47a3f79d22f04ed1bc8d22c35f664
sham52 <osamil.6152@gmail.com>
2023-04-09 20:34:18 +0300

    About section is completed

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
 
-export default Info
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">3 months internship</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt  about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">5 Project</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-support  about__icon"></i>
+
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
