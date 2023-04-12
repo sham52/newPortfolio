4ecfdfa42655eeb7a91e0084992135f37c013fcc
sham52 <osamil.6152@gmail.com>
2023-04-12 18:05:18 +0300

    Work section has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
+import Work from "./components/work/Work";
 function App() {
   return (
     <>
@@ -18,6 +19,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Testimonials />
         <Contact />
       </main>
---
+++ src/components/work/Work.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Works from "./Works";
+import "./work.css";
+
+const Work = () => {
+  return (
+    <section className="work" id="portfolio">
+      <div className="portfolio__header">
+        <h2 className="section__title">Portfolio</h2>
+        <span className="section__subtitle">Most recent works</span>
+      </div>
+
+      <Works />
+    </section>
+  );
+};
+
+export default Work;
