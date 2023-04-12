6fe03c25ba8bfa3b668af9a185c348a260a8a5b6
sham52 <osamil.6152@gmail.com>
2023-04-12 11:38:39 +0300

    Scrollup component has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 function App() {
   return (
     <>
@@ -19,8 +20,10 @@ function App() {
         <Qualification />
         <Testimonials />
         <Contact />
-        <Footer/>
       </main>
+
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
